fda630dd233739206699e03df2365d3ff0fe3c7e
keiohigh2nd <keiohigh2nd@gmail.com>
2014-04-16 22:47:49 +0900

    cross validate

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-This program is for gene network inferrence using Deep Belief Networks
+This program is for gene network inferrence using Deep Belief Networks and Stacked Autoencoder
+
+1:Prepare files
+  --first devide csv file into 1 and 0(devide_csv_file.py)
+  --get_samples_How many genes do you use? How much ratio for test?(get_samples.py)
+  --dbn_200_all_sort.py, sda.py, sda_dropout.py 
+  --Above are mainly compiled scripts
--- get_samples.py
+++ get_samples.py
@@ -1,5 +1,7 @@
 import random
 
+#This script makes random dataset(rand_**Network.csv) from **_Network.csv
+
 def get_sample(num, max, file):
   f = open(file)
   lines = f.readlines()
@@ -15,7 +17,33 @@ def get_sample(num, max, file):
     	f1.write(lines[tmp])
 
   f1.close()
-    
+
+def make_train_and_test(ratio, file):
+  tmp_file = "rand_" + file
+  f = open(tmp_file)
+  lines = f.readlines()
+  f.close()
+
+  test_file = "test_" + tmp_file
+  test = open(test_file, "w")
+
+  train_file = "train_" + tmp_file
+  train = open(train_file, "w")
+
+  i = 0
+  max = len(lines)*ratio
+  print max
+  for i in xrange(len(lines)):
+	if i < max:
+		train.write(lines[i])
+	else:
+		test.write(lines[i])
+
+  test.close()
+  train.close()      
+
 if __name__ == "__main__":
-  get_sample(100,400,"1_DREAM5_NetworkInference_GoldStandard_Network3.csv")
-  
+  #get_sample(100,400, "1_DREAM5_NetworkInference_GoldStandard_Network3.csv")
+  #get_sample(100,400, "0_DREAM5_NetworkInference_GoldStandard_Network3.csv")
+ 
+  make_train_and_test(0.8, "1_DREAM5_NetworkInference_GoldStandard_Network3.csv") 
